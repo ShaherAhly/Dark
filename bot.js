d86d05440f250fdc71e925392441306d1a32e469
ShaherAhly <42422914+ShaherAhly@users.noreply.github.com>
2018-09-10 21:14:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '.') {
-        message.reply('**Welcome To Dark Server :cocktail:**)
+        message.reply('**Welcome To Dark Server :cocktail:**')
 ');
       }
 });
