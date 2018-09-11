5757744acedc7283087d1b35fd24dd1b60e71b30
ShaherAhly <42422914+ShaherAhly@users.noreply.github.com>
2018-09-11 21:39:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,7 +162,7 @@ client.on('guildMemberAdd', member => {
     .setDescription(`اهلا بك في السيرفر`)
     .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
     .setColor('GREEN')
-    .setFooter('Dark System', 'https://cdn.discordapp.com/avatars/469862705273241630/5898dc71bb811771be4f9f58898fca95.jpg?size=128')
+    .setFooter('Black Night System', 'https://cdn.discordapp.com/avatars/469862705273241630/5898dc71bb811771be4f9f58898fca95.jpg?size=128')
 
 var channel =member.guild.channels.find('name', 'dark')
 if (!channel) return;
