41aaa4dd3097bf30ef7727dcf4e40926e0c83a02
ShaherAhly <42422914+ShaherAhly@users.noreply.github.com>
2018-10-02 10:55:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,6 @@ m.sendMessage(args)
 })
 }
 });
-
 client.on('message', message => {
 var prefix = "!";
       if(message.content === prefix + "hchannel") {
@@ -48,5 +47,30 @@ var prefix = "!";
               message.channel.send('Channel Hided Successfully ! ✅  ')
  }
 });
+
+client.on('message', message => {
+     if (message.author.bot) return;
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription(" تم أرسال الرابط برسالة خاصة ")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription(" مدة الرابط : ساعه  عدد استخدامات الرابط : 1 ")
+      message.author.sendEmbed(Embed11)
+    }
 
 client.login(process.env.BOT_TOKEN);
