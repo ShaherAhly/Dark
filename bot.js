79a4416f20d89fab2885b0b3054255df705e68d1
ShaherAhly <42422914+ShaherAhly@users.noreply.github.com>
2018-10-04 16:40:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('.bcall')){
- if (message.author.id !== '389090790984515594') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if (message.author.id !== '360095764535312385') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
